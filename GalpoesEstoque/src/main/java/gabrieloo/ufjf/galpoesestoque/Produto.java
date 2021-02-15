8b52c83a626c3fa41bbd23e030b855d5fdfd5765
fdellao <dellao@ice.ufjf.br>
2021-02-15 17:41:45 -0300

    Atributos privados, getters e setters protegidos

--- GalpoesEstoque/src/main/java/gabrieloo/ufjf/galpoesestoque/Produto.java
+++ GalpoesEstoque/src/main/java/gabrieloo/ufjf/galpoesestoque/Produto.java
@@ -9,6 +9,65 @@ package gabrieloo.ufjf.galpoesestoque;
  *
  * @author gabriel
  */
-public class Produto {
-    
+public abstract class Produto {
+	private int id, garantia;
+	private float preco, margem, peso, volume;
+	private String marca, modelo, estado, cor;
+
+	protected int getId(){
+		return id;
+	}
+
+	protected void setId(int id){
+		this.id = id;
+	}	
+	
+	protected int setGarantia(int garantia){
+		this.garantia = garantia;
+	}
+	
+	protected int getGarantia(){
+		return garantia;
+	}
+
+	protected void setPreco(float preco){
+		this.preco = preco;
+	}
+	
+	protected float getPreco(){
+		return preco;
+	}
+	
+	protected void setMargem(float margem){
+		this.margem = margem;
+	}
+	
+	protected float getMargem(){
+		return margem;
+	}
+
+	protected void setPeso(float peso){
+		this.peso = peso;
+	}
+	
+	protected float getPeso(){
+		return peso;
+	}
+
+	protected void setVolume(float volume){
+		this.volume = volume;
+	}
+	
+	protected float getVolume(){
+		return volume;
+	}
+
+    protected void setMarca(float marca){
+		this.marca = marca;
+	}
+	
+	protected float getMarca(){
+		return marca;
+	}
+
 }
