bd24ca3de1ecbf8fb1d4d8cb19c3c65d8dc9885f
fdellao <dellao@ice.ufjf.br>
2021-03-10 22:45:08 -0300

    Autenticação por arquivo txt

---
+++ GalpoesEstoque/src/main/java/gabrieloo/ufjf/galpoesestoque/Autenticacao.java
@@ -0,0 +1,59 @@
+/*Autores:
+  Caio César Lima de Azevedo;
+  Fernando Dellão Menini;
+  Gabriel Martins da Costa Medeiros;
+  Vinícius Barbosa Varoto;
+  Weyder Luiz Gomes Gante.
+  */
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.DatatypeConverter;
+public class Autenticacao {
+public static String md5(String senha)
+{
+	try
+	{
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.update(senha.getBytes());
+		byte[] d = m.digest();
+		senha = DatatypeConverter.printHexBinary(d);
+	}
+	catch(NoSuchAlgorithmException e)
+	{
+		e.printStackTrace();
+	}
+	return senha;
+}
+public static void auth(String usuario, String senha) {
+
+String line = null;
+
+try
+{
+	BufferedReader scan = new BufferedReader(new FileReader("login.txt"));
+	while((line = scan.readLine()) != null){
+		String u = line.split(",")[0];
+		String p = line.split(",")[1];
+		senha = md5(senha);
+		System.out.println("Senha entrada: "+senha);
+		if(u.equals(usuario)){
+			if(p.equals(senha)){
+				System.out.println("Bem-vindo, "+u);
+				break;
+			}
+			else{
+				System.out.println("Senha incorreta");
+			}
+		}
+		else{
+			System.out.println("Usuário inválido!");
+		}
+	}
+}
+catch(IOException ex)
+{
+	System.out.println("Erro");
+}
+}
+}
--- GalpoesEstoque/src/main/java/gabrieloo/ufjf/galpoesestoque/Login.java
+++ GalpoesEstoque/src/main/java/gabrieloo/ufjf/galpoesestoque/Login.java
@@ -11,7 +11,7 @@ package gabrieloo.ufjf.galpoesestoque;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,7 +19,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 
-public class Login extends JFrame
+public class Login extends JFrame implements ActionListener
 {
     public  Login()
     {
@@ -46,7 +46,6 @@ public class Login extends JFrame
 
         JLabel senhaUsuario = new JLabel("SENHA: ");
 		JTextField campoSenha = new JPasswordField(40);
-        //JTextField campoSenha = new JTextField(40);
 
         usuario.add(nomeUsuario);
         usuario.add(campoUsuario);
@@ -55,12 +54,17 @@ public class Login extends JFrame
 
         JPanel botoes = new JPanel();
         panelLogin.setLayout(new FlowLayout());
-
         JButton botaoEntrar = new JButton("Entrar");
+		botaoEntrar.addActionListener(new Login());
         botoes.add(botaoEntrar);
 
         add(panelLogin, BorderLayout.NORTH);
         add(usuario, BorderLayout.CENTER);
         add(botoes, BorderLayout.SOUTH);
     }
+	@override
+	public static void actionPerformed(ActionEvent e) {
+		Autenticacao.auth(campoUsuario.getText(), campoSenha.getText());
+
+	}
 }
